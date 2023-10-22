33f6059898392c9da91e516399726fb2a1b05fd5
Pouyan Madanipour <pouyan.mad@gmail.com>
2023-10-22 08:43:28 +0330

    update: screensize menu

--- components/Menu.js
+++ components/Menu.js
@@ -13,6 +13,7 @@ import ColorLensIcon from "@mui/icons-material/ColorLens";
 export default function Menu() {
   const { menuMobile, setMenuMobile } = useContext(StateContext);
   const { navigationTopBar, setNavigationTopBar } = useContext(StateContext);
+  const { screenSize, setScreenSize } = useContext(StateContext);
   const { menuColor, setMenuColor } = useContext(StateContext);
   const [desktop, setDesktop] = useState(false);
   const [colorPicker, setColorPicker] = useState(false);
@@ -54,45 +55,8 @@ export default function Menu() {
       className={classes.container}
       style={{ background: menuColor.background, color: menuColor.text }}
     >
-      <div className={classes.largeMenu}>
-        <Image
-          className={classes.logo}
-          width={120}
-          height={50}
-          src={logo}
-          alt="logo"
-          onClick={() => window.location.assign("/")}
-          priority
-        />
-        <div className={classes.largeNavigation}>
-          {navigationTopBar
-            .map((nav, index) => (
-              <a
-                key={index}
-                className={!nav.active ? classes.nav : classes.navActive}
-                onClick={() => activateNav(nav.link, index)}
-              >
-                {nav.title}
-                {nav.title === "" && <SearchIcon sx={{ fontSize: 30 }} />}
-              </a>
-            ))
-            .reverse()}
-        </div>
-        <div className={classes.colorPickerContainer}>
-          <ColorLensIcon
-            className="icon"
-            onClick={() => setColorPicker(!colorPicker)}
-            sx={{ color: "#ffcb08" }}
-          />
-          {colorPicker && (
-            <div className={classes.colorPicker}>
-              <CompactPicker onChangeComplete={handleChangeComplete} />
-            </div>
-          )}
-        </div>
-      </div>
-      <div className={classes.smallMenu}>
-        <div className={classes.topBar}>
+      {screenSize !== "mobile" && (
+        <div className={classes.largeMenu}>
           <Image
             className={classes.logo}
             width={120}
@@ -102,53 +66,96 @@ export default function Menu() {
             onClick={() => window.location.assign("/")}
             priority
           />
-          {menuMobile ? (
-            <CloseIcon
-              className={classes.menuIcon}
-              onClick={() => setMenuMobile(!menuMobile)}
-              sx={{ fontSize: 30 }}
+          <div className={classes.largeNavigation}>
+            {navigationTopBar
+              .map((nav, index) => (
+                <a
+                  key={index}
+                  className={!nav.active ? classes.nav : classes.navActive}
+                  onClick={() => activateNav(nav.link, index)}
+                >
+                  {nav.title}
+                  {nav.title === "" && <SearchIcon sx={{ fontSize: 30 }} />}
+                </a>
+              ))
+              .reverse()}
+          </div>
+
+          <div className={classes.colorPickerContainer}>
+            <ColorLensIcon
+              className="icon"
+              onClick={() => setColorPicker(!colorPicker)}
+              sx={{ color: "#ffcb08" }}
             />
-          ) : (
-            <MenuIcon
-              className={classes.menuIcon}
-              onClick={() => setMenuMobile(!menuMobile)}
-              sx={{ fontSize: 30 }}
+            {colorPicker && (
+              <div className={classes.colorPicker}>
+                <CompactPicker onChangeComplete={handleChangeComplete} />
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+
+      {screenSize === "mobile" && (
+        <div className={classes.smallMenu}>
+          <div className={classes.topBar}>
+            <Image
+              className={classes.logo}
+              width={120}
+              height={50}
+              src={logo}
+              alt="logo"
+              onClick={() => window.location.assign("/")}
+              priority
             />
+            {menuMobile ? (
+              <CloseIcon
+                className={classes.menuIcon}
+                onClick={() => setMenuMobile(!menuMobile)}
+                sx={{ fontSize: 30 }}
+              />
+            ) : (
+              <MenuIcon
+                className={classes.menuIcon}
+                onClick={() => setMenuMobile(!menuMobile)}
+                sx={{ fontSize: 30 }}
+              />
+            )}
+          </div>
+          {menuMobile && (
+            <Fragment>
+              <div
+                className={`${classes.menuMobile} animate__animated animate__slideInDown`}
+              >
+                <div>
+                  {navigationTopBar.map((nav, index) => (
+                    <a
+                      key={index}
+                      className={!nav.active ? classes.nav : classes.navActive}
+                      onClick={() => activateNav(nav.link, index)}
+                    >
+                      {nav.title}
+                      {nav.title === "" && <SearchIcon sx={{ fontSize: 30 }} />}
+                    </a>
+                  ))}
+                  {desktop && (
+                    <div className={classes.nav}>
+                      <p
+                        onClick={() => {
+                          Router.push("/download");
+                          setMenuMobile(false);
+                        }}
+                      >
+                        راهنمای نصب
+                      </p>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </Fragment>
           )}
         </div>
-        {menuMobile && (
-          <Fragment>
-            <div
-              className={`${classes.menuMobile} animate__animated animate__slideInDown`}
-            >
-              <div>
-                {navigationTopBar.map((nav, index) => (
-                  <a
-                    key={index}
-                    className={!nav.active ? classes.nav : classes.navActive}
-                    onClick={() => activateNav(nav.link, index)}
-                  >
-                    {nav.title}
-                    {nav.title === "" && <SearchIcon sx={{ fontSize: 30 }} />}
-                  </a>
-                ))}
-                {desktop && (
-                  <div className={classes.nav}>
-                    <p
-                      onClick={() => {
-                        Router.push("/download");
-                        setMenuMobile(false);
-                      }}
-                    >
-                      راهنمای نصب
-                    </p>
-                  </div>
-                )}
-              </div>
-            </div>
-          </Fragment>
-        )}
-      </div>
+      )}
     </div>
   );
 }
