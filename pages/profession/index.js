75b1015736d3643c1ac620b762136a251582df11
Pouyan Madanipour <pouyan.mad@gmail.com>
2024-08-18 12:06:00 +0330

    update: profession text

--- pages/profession/index.js
+++ pages/profession/index.js
@@ -10,6 +10,7 @@ import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 export default function Profession() {
   const { language, setLanguage } = useContext(StateContext);
 
+  const position = { position: "relative", top: language ? "8px" : "12px" };
   return (
     <Fragment>
       <NextSeo
@@ -43,9 +44,10 @@ export default function Profession() {
                 fontFamily: "FarsiBold",
               }}
             >
-              همچنان که گفته شد ما در اشاره شرکتی تمام خدمت هستیم. اما در ارائه
-              این خدمات آنچه اشاره را متمایز و متفاوت می‌کند و خدمت رسانی ما را
-              موثرتر و کارآمدتر می‌کند مبتنی بر چند اصل است؛
+              آژانس تبلیغاتی اشاره، آژانسی است که خدمات متنوع و گسترده‌ای را
+              ارائه می‌دهد و به معنای واقعی کلمه، آژانسی تمام‌خدمت است.
+              خدمت‌رسانی ما در اشاره هم متمایز است و هم کارآمد، چون کار ما مبتنی
+              بر اصول زیر است:
             </h3>
           ) : (
             <h3
@@ -53,173 +55,29 @@ export default function Profession() {
                 fontFamily: "EnglishMedium",
               }}
             >
-              As a full-service company, what differentiates and sets us apart
-              and enhances the effectiveness of our services is grounded in
-              couple of core principles:
+              Eshareh Advertising Agency is an agency which offers a whole array
+              of services and in the true sense of the word is a "full-service"
+              agency. Our service provision at Eshareh is both unique and
+              effective since it is grounded in couple of core principles:
             </h3>
           )}
         </div>
-        <div
-          className={
-            language ? classes.information : classes.informationReverse
-          }
-        >
-          {language ? (
-            <h3
-              style={{
-                fontFamily: "FarsiLight",
-              }}
-            >
-              ما باورمند به نوعی از رابطه با مشتریانمان هستیم که شراکتی راهبردی
-              مبتنی بر رابطه‌ی برد، برد، بلندمدت و وفادارانه است.
-            </h3>
-          ) : (
-            <h3
-              style={{
-                fontFamily: "EnglishLight",
-              }}
-            >
-              We cultivate strategic partnerships with our clients, founded on a
-              win-win, long-term and loyal relationship.
-            </h3>
-          )}
-        </div>
-        <div
-          className={
-            language ? classes.information : classes.informationReverse
-          }
-        >
-          {language ? (
-            <h3
-              style={{
-                fontFamily: "FarsiLight",
-              }}
-            >
-              در تدوین و مدیریت کمپین‌های تبلیغاتی، خلاقیت محور اصلی تمام زیر و
-              بم کار ما و راهکارهایی است که به شرکایمان ارائه می‌دهیم. این
-              خلافیت را اما همیشه به روز نگه داشته و همپا با استانداردهای جهانی
-              و متصل به تجربه و شناخت استراتژیکمان از بازار کسب و کار ایران
-              تعریف کرده‌ایم.
-            </h3>
-          ) : (
-            <h3
-              style={{
-                fontFamily: "EnglishLight",
-              }}
-            >
-              When designing and managing advertising campaigns, creativity is
-              at the heart of everything we do, driving the innovative solutions
-              we deliver to our partners. However, we continuously refine our
-              distinct creative approach to align with global standards, drawing
-              on our extensive experience and strategic insights into the
-              Iranian business market.
-            </h3>
-          )}
-        </div>
-        <div
-          className={
-            language ? classes.information : classes.informationReverse
-          }
-        >
-          {language ? (
-            <h3
-              style={{
-                fontFamily: "FarsiLight",
-              }}
-            >
-              آموزش را یکی از ارکان هویتی سازمانمان می‌دانیم. در این مسیر برای
-              همکاران و شرکای تجاری‌مان به طور مستمر دوره‌های آموزشی و
-              ورکشاپ‌های تخصصی برگزار می‌کنیم. در این مسیر متکی به دانش روز
-              جهانی صنعت تبلیغات و بازاریابی هستیم و اساتید ممتاز این حوزه‌ها
-              همراهی‌مان می‌کنند.
-            </h3>
-          ) : (
-            <h3
-              style={{
-                fontFamily: "EnglishLight",
-              }}
-            >
-              Education is a cornerstone of our organisation's spirit, which is
-              why we consistently organise training courses and specialised
-              workshops for our colleagues and business partners and for this,
-              we highly rely on up-to-date global advertising and marketing
-              knowledge and we are accompanied by best-in-class tutors and
-              mentors in these fields.
-            </h3>
-          )}
-        </div>
-        <div
-          className={
-            language ? classes.information : classes.informationReverse
-          }
-        >
-          {language ? (
-            <h3
-              style={{
-                fontFamily: "FarsiLight",
-              }}
-            >
-              ما آژانسی تمام خدمت هستیم که به بسیاری از نیازهای مشتریانمان در
-              ارتباط با فعالیت‌های بازاریابی پاسخ داده و به آنان خدمت ارائه
-              می‌دهیم.
-            </h3>
-          ) : (
-            <h3
-              style={{
-                fontFamily: "EnglishLight",
-              }}
-            >
-              We are a full-service agency dedicated to addressing the diverse
-              marketing needs of our clients with comprehensive and responsive
-              solutions.
-            </h3>
-          )}
-        </div>
-        <div
-          className={
-            language ? classes.information : classes.informationReverse
-          }
-        >
-          {language ? (
-            <h3
-              style={{
-                fontFamily: "FarsiLight",
-              }}
-            >
-              سرویس‌های ما شامل:
-            </h3>
-          ) : (
-            <h3
-              style={{
-                fontFamily: "EnglishLight",
-              }}
-            >
-              Our services include:
-            </h3>
-          )}
-        </div>
+
         <div
           className={
             language ? classes.information : classes.informationReverse
           }
         >
           <div className={classes.row}>
-            <RadioButtonUncheckedIcon sx={{ fontSize: 10 }} />
+            <RadioButtonUncheckedIcon style={position} sx={{ fontSize: 10 }} />
             {language ? (
               <h3
                 style={{
                   fontFamily: "FarsiLight",
                 }}
               >
-                <span
-                  style={{
-                    fontFamily: "FarsiMedium",
-                  }}
-                >
-                  ارائه‌ی استراتژی ارتباطی،
-                </span>{" "}
-                تدوین کمپین‌های گسترده تبلیغاتی خلاق- تیزر تبلیغاتی، تیزر
-                تلویزیونی، تیزر رادیویی و تبلیغات محیطی.
+                تلاش و باور ما این است که رابطه ما با مشتریانمان، رابطه‌ای
+                بلندمدت، برد-برد و وفادارانه باشد.
               </h3>
             ) : (
               <h3
@@ -227,15 +85,8 @@ export default function Profession() {
                   fontFamily: "EnglishLight",
                 }}
               >
-                <span
-                  style={{
-                    fontFamily: "EnglishMedium",
-                  }}
-                >
-                  Communication strategy development,
-                </span>{" "}
-                crafting extensive creative advertising campaigns, television
-                and radio commercials, and out-of-home advertising.
+                We believe in and strive for a long-lasting, win-win
+                relationship with our clients.
               </h3>
             )}
           </div>
@@ -246,22 +97,18 @@ export default function Profession() {
           }
         >
           <div className={classes.row}>
-            <RadioButtonUncheckedIcon sx={{ fontSize: 10 }} />
+            <RadioButtonUncheckedIcon style={position} sx={{ fontSize: 10 }} />
             {language ? (
               <h3
                 style={{
                   fontFamily: "FarsiLight",
                 }}
               >
-                <span
-                  style={{
-                    fontFamily: "FarsiMedium",
-                  }}
-                >
-                  ارائه‌ی راه‌کارهای ارتباطی دیجیتال
-                </span>{" "}
-                و تدوین کمپین‌های این حوزه- تبلیغات آنلاین، تبلیغات شبکه‎‌های
-                اجتماعی، بازاریابی محتوا و شبکه های اجتماعی و تبلیغات بنری.
+                در تدوین و مدیریت کمپین‌های تبلیغاتی، خلاقیت محور اصلی
+                راهکارهایی است که به شرکایمان ارائه می‌دهیم. برای ما خلاقیت هیچ
+                موقع کهنه نمی‌شود و آن را با استانداردهای جهانی، تجربه خود و
+                شناخت استراتژیکی که از بازار کسب و کار ایران داریم، همراه
+                می‌کنیم.
               </h3>
             ) : (
               <h3
@@ -269,15 +116,11 @@ export default function Profession() {
                   fontFamily: "EnglishLight",
                 }}
               >
-                <span
-                  style={{
-                    fontFamily: "EnglishMedium",
-                  }}
-                >
-                  Digital communication solutions,
-                </span>{" "}
-                crafting campaigns including online advertising, social media
-                advertising, content and social media marketing, and banner ads.
+                When designing and managing advertising campaigns, creativity is
+                at the heart of the solutions we deliver to our partners. For
+                us, creativity never loses novelty as we continuously align it
+                with global standards, our expertise and our strategic insights
+                into the Iranian business market.
               </h3>
             )}
           </div>
@@ -288,31 +131,66 @@ export default function Profession() {
           }
         >
           <div className={classes.row}>
-            <RadioButtonUncheckedIcon sx={{ fontSize: 10 }} />
+            <RadioButtonUncheckedIcon style={position} sx={{ fontSize: 10 }} />
             {language ? (
               <h3
                 style={{
                   fontFamily: "FarsiLight",
                 }}
               >
+                آموزش را یکی از ارکان هویتی سازمانی‌مان می‌دانیم و در این مسیر
+                برای همکاران و شرکای تجاری‌مان به طور مستمر دوره‌های آموزشی و
+                ورکشاپ‌های تخصصی برگزار می‌کنیم. ما متکی به دانش روز صنعت
+                بازاریابی و تبلیغات هستیم و  اساتید ممتاز این حوزه‌ها همراهی‌مان
+                می‌کنند.
+              </h3>
+            ) : (
+              <h3
+                style={{
+                  fontFamily: "EnglishLight",
+                }}
+              >
+                Education is a cornerstone of our organization's spirit, which
+                is why we consistently organize training courses and specialized
+                workshops for our colleagues and business partners and for this,
+                we highly rely on up-to-date global advertising and marketing
+                knowledge and we are accompanied by best-in-class tutors and
+                mentors in these fields.
+              </h3>
+            )}
+          </div>
+        </div>
+        <div
+          className={
+            language ? classes.information : classes.informationReverse
+          }
+        >
+          <div className={classes.row}>
+            <RadioButtonUncheckedIcon style={position} sx={{ fontSize: 10 }} />
+            {language ? (
+              <h3
+                style={{
+                  fontFamily: "FarsiLight",
+                }}
+              >
+                ما آژانسی تمام‌خدمت هستیم که به بسیاری از نیازهای مشتریانمان در
+                ارتباط با فعالیت‌های بازاریابی پاسخ داده و به آنان خدمت ارائه
+                می‌دهیم. سرویس‌های ما شامل ارائه استراتژی ارتباطی، تدوین
+                کمپین‌های گسترده تبلیغاتی خلاق (تیزر تبلیغاتی، تیزر تلویزیونی،
+                تیزر رادیویی، تبلیغات محیطی و …)، ارائه راهکارهای ارتباطی
+                دیجیتال و تدوین کمپین‌های این حوزه (تبلیغات آنلاین، تبلیغات
+                شبکه‎‌های اجتماعی، بازاریابی محتوا و شبکه های اجتماعی، تبلیغات
+                بنری و …)، ارائه ‌راهکارهای رسانه و خرید رسانه تبلیغاتی و محیطی
+                و همین‌طور طراحی و تولید راهکارهایی در زمینه تبلیغات{" "}
                 <span
                   style={{
-                    fontFamily: "FarsiMedium",
-                  }}
-                >
-                  ارائه ‌راه‌کارهای رسانه
-                </span>{" "}
-                و خرید رسانه تبلیغاتی و محیطی و همینطور طراحی و تولید
-                راه‌کارهایی در زمینه تبلیغات{" "}
-                <span
-                  style={{
-                    fontFamily: "EnglishLight",
+                    fontFamily: "English",
                   }}
                 >
                   BTL
                 </span>{" "}
-                - محل فروش، سمپلینگ، کمپین‌های نمایشگاهی، غرفه سازی و برگزاری
-                رویداد است.
+                (محل فروش، سمپلینگ، کمپین‌های نمایشگاهی، غرفه‌سازی، برگزاری
+                رویداد و …) است.
               </h3>
             ) : (
               <h3
@@ -320,17 +198,18 @@ export default function Profession() {
                   fontFamily: "EnglishLight",
                 }}
               >
-                <span
-                  style={{
-                    fontFamily: "EnglishMedium",
-                  }}
-                >
-                  Media solutions,
-                </span>{" "}
-                advertising and outdoor media buying, designing and executing
-                Below the Line (BTL) campaigns including but not limited to
-                Point of Sale, sampling, holding exhibitions, booth
-                construction, and event organisation.
+                We are a full-service agency dedicated to addressing the diverse
+                marketing needs of our clients with comprehensive and responsive
+                solutions. Our services include communication strategy
+                development, developing extensive creative advertising
+                campaigns(television and radio commercials, and out-of-home
+                advertising, …), offering digital communication solutions and
+                developing digital campaigns(online advertising, social media
+                advertising, content and social media marketing, banner ads, …),
+                offering media solutions, advertising and outdoor media buying,
+                designing and executing Below the Line (BTL) campaigns including
+                but not limited to Point of Sale, sampling, holding exhibitions,
+                booth construction, and event organization.
               </h3>
             )}
           </div>
