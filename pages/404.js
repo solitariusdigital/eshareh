ee704a45fd3d57889ae13eae86e57979a16ba4c2
Pouyan Madanipour <pouyan.mad@gmail.com>
2024-08-20 13:04:44 +0330

    update: page not found

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,23 @@
+import { Fragment, useContext } from "react";
+import { StateContext } from "@/context/stateContext";
 import classes from "./home.module.scss";
 
 export default function NotFoundPage() {
+  const { language, setLanguage } = useContext(StateContext);
+
   return (
     <div className={classes.notFound}>
-      <h2>صفحه یافت نشد</h2>
-      <p>صفحه مورد نظر وجود ندارد</p>
+      {language ? (
+        <Fragment>
+          <h2>صفحه یافت نشد</h2>
+          <p>صفحه مورد نظر وجود ندارد</p>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <h2>Page not found</h2>
+          <p>The desired page does not exist</p>
+        </Fragment>
+      )}
     </div>
   );
 }
