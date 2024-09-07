beb7730da92559717715f7ea9fbba73c9ce06cc7
Pouyan Madanipour <pouyan.mad@gmail.com>
2024-09-07 16:06:54 +0330

    update: share icons

--- pages/charity/index.js
+++ pages/charity/index.js
@@ -4,10 +4,27 @@ import classes from "./charity.module.scss";
 import { NextSeo } from "next-seo";
 import { getCharityApi, updateCharityApi } from "@/services/api";
 import secureLocalStorage from "react-secure-storage";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import XIcon from "@mui/icons-material/X";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  LinkedinShareButton,
+  WhatsappShareButton,
+  TelegramShareButton,
+} from "react-share";
 
 export default function Charity() {
   const { language, setLanguage } = useContext(StateContext);
   const [charity, setCharity] = useState({});
+  const [buttonMessage, setButtonMessage] = useState("خیریه");
+
+  const shareUrl = "https://eshareh.com/charity";
+  const title = "کمپین خیریه اشاره";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,6 +50,8 @@ export default function Charity() {
       await updateCharityApi(dataObject);
       setCharity(dataObject);
       secureLocalStorage.setItem("charityUser", true);
+    } else {
+      setButtonMessage("مشارکت شما ثبت شد");
     }
   };
 
@@ -65,8 +84,60 @@ export default function Charity() {
           }}
           onClick={() => updateCharityCount()}
         >
-          خیریه
+          {buttonMessage}
         </button>
+        <div
+          className={classes.shareContainer}
+          onClick={() => navigator.clipboard.writeText(shareUrl)}
+        >
+          <LinkedinShareButton
+            className={classes.share}
+            url={shareUrl}
+            title={title}
+          >
+            <LinkedInIcon />
+          </LinkedinShareButton>
+          <TwitterShareButton
+            className={classes.share}
+            url={shareUrl}
+            title={title}
+          >
+            <XIcon />
+          </TwitterShareButton>
+          <WhatsappShareButton
+            className={classes.share}
+            url={shareUrl}
+            title={title}
+          >
+            <WhatsAppIcon />
+          </WhatsappShareButton>
+          <TelegramShareButton
+            className={classes.share}
+            url={shareUrl}
+            title={title}
+          >
+            <TelegramIcon
+              className={classes.share}
+              onClick={() =>
+                window.open("https://www.instagram.com/", "_ self")
+              }
+            />
+          </TelegramShareButton>
+          <FacebookShareButton
+            className={classes.share}
+            url={shareUrl}
+            quote={title}
+          >
+            <FacebookIcon />
+          </FacebookShareButton>
+          <div className={classes.share}>
+            <InstagramIcon
+              onClick={() =>
+                window.open("https://www.instagram.com/", "_ self")
+              }
+            />
+          </div>
+        </div>
       </div>
     </Fragment>
   );
