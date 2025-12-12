5b915bc55af56b04ec35f6b45fe083b7b2e7a68c
Pouyan Madanipour <pouyan.mad@gmail.com>
2025-12-12 22:08:58 +0330

    updates

--- pages/_document.js
+++ pages/_document.js
@@ -19,44 +19,8 @@ export default class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/public/icon-192x192.png"></link>
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="theme-color" content="#981b1e" />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-Z14DF4983K"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-Z14DF4983K');
-              `,
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){
-                  w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});
-                  var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                  j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                  f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-5TTNMPD9');
-              `,
-            }}
-          />
         </Head>
         <body>
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-5TTNMPD9"
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
-          </noscript>
           <Main />
           <NextScript />
         </body>
