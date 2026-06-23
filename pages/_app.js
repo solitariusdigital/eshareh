205db5f15767507af8cb12ef7327e9a91fcd4285
Pouyan Madanipour <pouyan.mad@gmail.com>
2026-06-23 11:05:11 +0330

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,16 @@ export default function App({ Component, pageProps }) {
     <StateProvider>
       <RootLayout>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, viewport-fit=cover"
+          />
+          <meta name="theme-color" content="#fdb714" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
         </Head>
         <Component {...pageProps} />
       </RootLayout>
