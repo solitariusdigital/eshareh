4ef86d153f5072b7a351b43da17e014e08d20fe4
Pouyan Madanipour <pouyan.mad@gmail.com>
2026-07-26 13:18:48 +0330

    updates

--- pages/nikibezabaneeshareh/index.js
+++ pages/nikibezabaneeshareh/index.js
@@ -4,9 +4,7 @@ import classes from "./nikibezabaneeshareh.module.scss";
 import Image from "next/legacy/image";
 import { NextSeo } from "next-seo";
 import logoFarsi from "@/assets/logoFarsi.svg";
-import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import esharehWhite from "@/assets/esharehWhite.svg";
-import { getCharityApi, updateCharityApi } from "@/services/api";
 
 export default function Nikibezabaneeshareh() {
   const { language, setLanguage } = useContext(StateContext);
@@ -15,8 +13,9 @@ export default function Nikibezabaneeshareh() {
   const { displayMenu, setDisplayMenu } = useContext(StateContext);
   const { screenSize, setScreenSize } = useContext(StateContext);
   const [displayInfo, setDisplayInfo] = useState(false);
+  const [displayVideo, setDisplayVideo] = useState(false);
 
-  const fullSizeChatBox =
+  const fullSizeScreen =
     screenSize === "desktop" || screenSize === "tablet-landscape";
 
   useEffect(() => {
@@ -34,21 +33,21 @@ export default function Nikibezabaneeshareh() {
       () => {
         setDisplayInfo(true);
       },
-      fullSizeChatBox ? 8500 : 7000,
+      fullSizeScreen ? 9000 : 7000,
     );
     return () => clearTimeout(timeoutId);
-  }, [screenSize, fullSizeChatBox]);
+  }, [fullSizeScreen]);
 
-  const updateCharityCount = async () => {
-    const dataCharity = await getCharityApi();
-    let count = dataCharity[0].nikibezabaneeshareh;
-    count += 1;
-    let dataObject = {
-      ...dataCharity[0],
-      nikibezabaneeshareh: count,
-    };
-    await updateCharityApi(dataObject);
-  };
+  useEffect(() => {
+    setDisplayVideo(false);
+    const timeoutId = setTimeout(
+      () => {
+        setDisplayVideo(true);
+      },
+      fullSizeScreen ? 11000 : 9000,
+    );
+    return () => clearTimeout(timeoutId);
+  }, [fullSizeScreen]);
 
   return (
     <Fragment>
@@ -73,122 +72,51 @@ export default function Nikibezabaneeshareh() {
         robots="index, follow"
       />
       <div className={classes.container}>
-        {fullSizeChatBox && (
-          <div className={classes.videoContainer}>
-            <video
-              className={classes.video}
-              src={
-                "https://bucket.eshareh.com/nikibezabaneeshareh/motion-desktop.mp4"
-              }
-              autoPlay
-              playsInline
-              preload="metadata"
-              muted="true"
-            />
-          </div>
-        )}
-        {!fullSizeChatBox && (
-          <div className={classes.videoContainer}>
-            {displayInfo && (
-              <h1
-                className={`${classes.title} animate__animated animate__slideInDown`}
-                style={{
-                  fontFamily: "FarsiFat",
-                }}
-              >
-                نیکی به زبان اشاره
-              </h1>
-            )}
-            <video
-              className={classes.video}
-              src={
-                "https://bucket.eshareh.com/nikibezabaneeshareh/motion-mobile.mp4"
-              }
-              autoPlay
-              playsInline
-              preload="metadata"
-              muted="true"
-            />
-          </div>
-        )}
+        <video
+          className={classes.backgroundVideo}
+          src={
+            fullSizeScreen
+              ? "https://bucket.eshareh.com/nikibezabaneeshareh/motion-desktop.mp4"
+              : "https://bucket.eshareh.com/nikibezabaneeshareh/motion-mobile.mp4"
+          }
+          preload="metadata"
+          autoPlay
+          playsInline
+          muted
+        />
         {displayInfo && (
           <div
-            className={`${classes.information} animate__animated ${
-              fullSizeChatBox ? "animate__slideInRight" : "animate__slideInUp"
-            }`}
+            className={`${classes.information} animate__animated animate__slideInDown`}
           >
-            {fullSizeChatBox && (
-              <h1
-                style={{
-                  fontFamily: "FarsiFat",
-                }}
-              >
-                نیکی به زبان اشاره
-              </h1>
-            )}
-            <p>
-              امسال در کمپین مسئولیت اجتماعی اشاره، به سراغ عزیزان ناشنوا و
-              کم‌شنوا رفته‌ایم تا از دریچه‌ای تازه به زندگی روزمره و چالش‌هایی
-              که با آن مواجه‌اند، بپردازیم؛ چرا که ما در اشاره باور داریم هر گام
-              کوچکی می‌تواند راه را برای تغییری بزرگ هموار کند. در این راستا
-              کمپینی طراحی شد با شعار «نیکی به زبان اشاره»
-            </p>
-            {screenSize !== "mobile" && <p>کمپینی که شامل سه بخش زیر است:</p>}
-            <div className={classes.row}>
-              <FiberManualRecordIcon sx={{ fontSize: "0.6vw" }} />
-              <p>تهیه و اهدای تعدادی دستگاه سمعک برای افرادکم‌شنوا</p>
-            </div>
-            <div className={classes.row}>
-              <FiberManualRecordIcon sx={{ fontSize: "0.6vw" }} />
-              <p>
-                طراحی و ارائه دفترچه فیزیکی ویژه ناشنوایان و کم‌شنوایان برای
-                آسان‌تر شدن ارتباط با آن‌ها
-              </p>
-            </div>
-            <div className={classes.row}>
-              <FiberManualRecordIcon sx={{ fontSize: "0.6vw" }} />
-              <p>
-                انتشار دفترچه آنلاین راهنمای برخورد با کم‌شنوایان/ناشنوایان برای
-                ارتقای سطح آگاهی عمومی و رسمیت‌بخشیدن به زبان اشاره به ‌عنوان
-                پلی برای برقراری ارتباط مؤثر
-              </p>
-            </div>
-            <p
+            <h1
               style={{
-                marginTop: "1vh",
+                fontFamily: "FarsiFat",
               }}
             >
-              با دانلود و مطالعه دفترچه آنلاین، نه‌ تنها سهمی در گسترش این آگاهی
-              خواهید داشت، بلکه به ازای هر دانلود، مبلغی نیز برای تهیه سمعک‌ به
-              کم‌شنوایان اختصاص خواهد یافت.
-            </p>
-            <h2>در نیکــــی به زبان اشـــــاره هـــمراه ﺷــــﻮﯾﺪ.</h2>
-            <a
-              className={classes.button}
-              onClick={async () => {
-                updateCharityCount();
-                const fileUrl =
-                  "https://bucket.eshareh.com/nikibezabaneeshareh/دفترچه_ارتباط_با_ناشنوایان_و_کم_شنوایان.pdf";
-                const fileName = "دفترچه_ارتباط_با_ناشنوایان_و_کم_شنوایان.pdf";
-                window.open(fileUrl, "_blank", "noopener,noreferrer");
-                try {
-                  const response = await fetch(fileUrl);
-                  const blob = await response.blob();
-                  const blobUrl = window.URL.createObjectURL(blob);
-                  const link = document.createElement("a");
-                  link.href = blobUrl;
-                  link.download = fileName;
-                  document.body.appendChild(link);
-                  link.click();
-                  document.body.removeChild(link);
-                  window.URL.revokeObjectURL(blobUrl);
-                } catch (err) {
-                  console.error("File download failed:", err);
-                }
-              }}
-            >
-              <h2 style={{ fontFamily: "FarsiBold" }}>به نیکی اشاره کنید</h2>
-            </a>
+              نیکی به زبان اشاره
+            </h1>
+            <h3>
+              با همراهی و به نمایندگی از همه شما، صدای زندگی را به گوش تعدادی از
+              کم‌شنواهای بی‌بضاعت سراسر ایران رساندیم؛ اما پشت این اتفاق، قصه‌ای
+              است از ایده‌ها، آدم‌ها و لحظه‌هایی که ما را در ساختن این نیکی یاری
+              کردند. با تماشای ویدیوی زیر، قصه این نیکی را از زبان اشاره بشنوید.
+            </h3>
+          </div>
+        )}
+        {displayVideo && (
+          <div
+            className={`${classes.videoWrapper} animate__animated animate__slideInUp`}
+          >
+            <video
+              className={classes.video}
+              src="https://bucket.eshareh.com/nikibezabaneeshareh/The-Making-of-Kindness-Campaign.mp4"
+              poster="https://bucket.eshareh.com/page/home/cover.jpg"
+              playsInline
+              preload="metadata"
+              controls
+              // autoPlay
+              // muted
+            />
           </div>
         )}
         <div
